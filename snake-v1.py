1a491b5a272aeec8bf564f720cd2a3f65f2bb3c0
Shinlen06 <124433317+Shinlen06@users.noreply.github.com>
2023-04-04 21:22:15 +0200

    Update snake-v1.py

--- snake-v1.py
+++ snake-v1.py
@@ -1,10 +1,35 @@
-from tkinter import * # Importation de la bibliothèque  Tkinter
+# -*- coding: utf-8 -*-
+"""
+Programme Snake
+
+"""
+from tkinter import * # Importation de la bibliothèque  Tkinter 
 from tkinter import font as tkfont
 from random import randint
 
+from PIL import Image, ImageTk 
 # On crée un environnement Tkinter
 tk = Tk()
-
+im_teteN = Image.open("teteN.png") 
+teteN = ImageTk.PhotoImage(im_teteN) 
+im_teteS = Image.open("teteS.png") 
+teteS = ImageTk.PhotoImage(im_teteS) 
+im_teteE = Image.open("teteE.png") 
+teteE = ImageTk.PhotoImage(im_teteE) 
+im_teteW = Image.open("teteW.png") 
+teteW = ImageTk.PhotoImage(im_teteW) 
+im_noeud1 = Image.open("noeud1.png") 
+noeud1 = ImageTk.PhotoImage(im_noeud1) 
+im_noeud2 = Image.open("noeud2.png") 
+noeud2 = ImageTk.PhotoImage(im_noeud2)
+pomme = Image.open("pomme.png") 
+pomme = ImageTk.PhotoImage(pomme)
+pomme2 = Image.open("Pomme2.png")
+pomme2 = ImageTk.PhotoImage(pomme2)
+pomme3 = Image.open("Pomme3.png")
+pomme3 = ImageTk.PhotoImage(pomme3)
+score=0
+vitesse=100
 def right(event):
     # Modification de la variable globale direction
     global direction
@@ -24,17 +49,19 @@ def up(event):
     # Modification de la variable globale direction
     global direction
     direction = 'up'
+    
 
 # Calcule la nouvelle frame de jeu
-def computeNextFrame(numFrame,coordonnee, objet):
+def computeNextFrame(numFrame,coordonnee, objet, objet_2,objet_3):
     global direction
+    global score
+    global vitesse
+    mort = False
     # Affiche le numérod de la frame
     #print(numFrame)
     numFrame = numFrame + 1
-    
     # Effacer le canevas
     can.delete('all')
-    
     # Propagation du déplacement des noeuds
     for n in range (len(coordonnee)-1,0,-1):
         coordonnee[n][0] = coordonnee[n-1][0]
@@ -43,39 +70,42 @@ def computeNextFrame(numFrame,coordonnee, objet):
     # Mise à jour des coordonnées
     if direction == 'right':
         coordonnee[0][0] += 20
+        can.create_image(coordonnee[0][0], coordonnee[0][1], anchor = NW, image = teteE)
         if coordonnee[0][0] > 480:
             coordonnee[0][0] = 0
-    if direction == 'left':
+    elif direction == 'left':
         coordonnee[0][0] += -20
+        can.create_image(coordonnee[0][0], coordonnee[0][1], anchor = NW, image = teteW)
         if coordonnee[0][0] < 0:
             coordonnee[0][0] = 480
-    if direction == 'up':
+    elif direction == 'up':
         coordonnee[0][1] += -20
+        can.create_image(coordonnee[0][0], coordonnee[0][1], anchor = NW, image = teteN)
         if coordonnee[0][1] < 0:
             coordonnee[0][1] = 480
-    if direction == 'down':
+    elif direction == 'down':
         coordonnee[0][1] += 20
+        can.create_image(coordonnee[0][0], coordonnee[0][1], anchor = NW, image = teteS)
         if coordonnee[0][1] > 480:
             coordonnee[0][1] = 0
 
-    # Dessin de la tête du serpent et de noeuds
-    can.create_rectangle(coordonnee[0][0], coordonnee[0][1], coordonnee[0][0] + 20,
-                         coordonnee[0][1] + 20, outline='yellow', fill='red')
     
     for n in range(1,len(coordonnee)):
-        if n%2 == 0:
-            ligne = 'blue'
-            couleur = 'green'
+        if n%2 == 0: 
+            can.create_image(coordonnee[n][0], coordonnee[n][1], anchor = NW, image = noeud1)
         else:
-            ligne = 'green'
-            couleur = 'blue'
-        can.create_rectangle(coordonnee[n][0], coordonnee[n][1], coordonnee[n][0] + 20,
-                         coordonnee[n][1] + 20, outline= ligne, fill= couleur)    
+            can.create_image(coordonnee[n][0], coordonnee[n][1], anchor = NW, image = noeud2)
+        
     # Dessine les objets
     for p in range(len(objet)):
-        can.create_oval(objet[p][0], objet[p][1], objet[p][0] + 20,
-                         objet[p][1] + 20, outline= 'red', fill= 'green')   
-        
+        can.create_image(objet[0][0], objet[0][1], anchor = NW, image = pomme)   
+    
+    for p in range(len(objet_2)):
+        can.create_image(objet_2[0][0], objet_2[0][1], anchor = NW, image = pomme2)   
+    
+    for p in range(len(objet_3)):
+        can.create_image(objet_3[0][0], objet_3[0][1], anchor = NW, image = pomme3)
+    
     for p in range(len(objet)):
         if coordonnee[0][0] == objet [0][0] and coordonnee[p][1] == objet [p][1]:
             # Déplacement de la pomme
@@ -83,23 +113,48 @@ def computeNextFrame(numFrame,coordonnee, objet):
             objet[0][1] = randint(1,24)* 20
             # Ajout d'un noeud au serpent (à la même place que le dernier noeud)
             coordonnee.append([-20, -20]) # Caché pour l'instant
-
+            score=score+5
+    
+    for p in range(len(objet_2)):
+        if coordonnee[0][0] == objet_2 [0][0] and coordonnee[p][1] == objet_2 [p][1]:
+            # Déplacement de la pomme
+            objet_2[0][0] = randint(1,24)* 20
+            objet_2[0][1] = randint(1,24)* 20
+            # Ajout d'un noeud au serpent (à la même place que le dernier noeud)
+            coordonnee.append([-20, -20])
+            score=score+15
+            vitesse = vitesse-7
+    
+    for p in range(len(objet_3)):
+        if coordonnee[0][0] == objet_3 [0][0] and coordonnee[p][1] == objet_3 [p][1]:
+            mort = True
+    
+    for p in range(len(objet_3)):
+        can.create_image(objet_3[0][0], objet_3[0][1], anchor = NW, image = pomme3)
+    
     game_over = False     
     # On test la position de la tête par rapport aux noeuds du serpent
     for n in range(1,len(coordonnee)): # L'indice 0 est exclu, c'est la tête
         if coordonnee[0][0] == coordonnee [n][0] and coordonnee[p][1] == coordonnee [n][1]:
-            game_over = True # La partie est finie
-            
-    if game_over :
+            game_over = True # La partie est finie        
+        if mort == True:
+            game_over = True
+    
+    if game_over : 
         # Fin de partie
         TEXTE = "GAME OVER"
+        score=str(score)
         normal_font = tkfont.Font(family="Helvetica", size=12, weight="bold")
         can.create_text(100,200,text = TEXTE, fill='red',  font=normal_font)
+        normal_font = tkfont.Font(family="Helvetica", size=12, weight="bold")
+        can.create_text(100,300,text = "score = "+score, fill='red',  font=normal_font)
+    
     else:
         # La partie n'est pas finie
         # Calcule une nouvelle frame dans 100 ms
-        tk.after(100, lambda:computeNextFrame(numFrame,coordonnee, objet))
-
+        tk.after(vitesse, lambda:computeNextFrame(numFrame,coordonnee, objet , objet_2, objet_3))
+    
+    
 
 if __name__ == "__main__":
     # On crée un canevas dans l'environnement Tkinter d'une taille de 500x500
@@ -113,7 +168,7 @@ if __name__ == "__main__":
     can.pack()
     
     # Direction par défaut
-    direction = 'up'
+    direction = 'up' 
     
     coordonnee = [ [200, 200], [200, 220], [200, 240], [220, 240] ]
     objet = []
@@ -123,14 +178,26 @@ if __name__ == "__main__":
     y = randint(1,24)
     objet.append([x*20, y*20, 0])
     
+    # Second objet (la pomme 2)
+    objet_2=[]
+    a = randint(1,24)
+    b = randint(1,24)
+    objet_2.append([a*20, b*20, 0])
+    
+    # Second objet (la pomme empoisonnée)
+    objet_3=[]
+    c = randint(1,24)
+    d = randint(1,24)
+    objet_3.append([c*20, d*20, 0])
+    
     # Construction de la première étape de simulation
-    computeNextFrame(0,coordonnee, objet)
+    computeNextFrame(0,coordonnee, objet, objet_2,objet_3)
     
     # Appuyer sur la touche 'd' appellera la fonction right()
-    tk.bind('<d>', right)
-    tk.bind('<q>', left)
-    tk.bind('<s>', down)
-    tk.bind('<z>', up)
+    tk.bind('<d>', right) 
+    tk.bind('<q>', left) 
+    tk.bind('<s>', down) 
+    tk.bind('<z>', up) 
     
     # lancement de la boucle principale qui écoute les évènements (claviers...)
     tk.mainloop() # Cet appel doit être la derniere instruction du programme
